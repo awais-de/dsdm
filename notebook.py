745ec15978c279a4536946daea21a3befed2c9a5
Muhammad Awais <awais.basharat.va@razor-group.com>
2024-07-11 13:31:53 +0200

    Completed script

--- notebook.py
+++ notebook.py
@@ -1,19 +1,22 @@
 import pandas as pd
 from googleapiclient.discovery import build
 
-def get_video_links(api_key, query, max_results=10):
-    youtube = build('youtube', 'v3', developerKey=api_key)
+api_key = 'AIzaSyCDYMgkKyBXWOYCSR9BBcIAkKF0TKaWibg'  # Replace with your actual API key
+youtube = build('youtube', 'v3', developerKey=api_key)
+max_results = 500
 
-    request = youtube.search().list(
+
+def get_video_links(query, channel_id, max_results=10):
+    video_links = []
+
+    response = youtube.search().list(
         q=query,
         part='snippet',
         type='video',
-        maxResults=max_results
-    )
-    
-    response = request.execute()
+        #maxResults=max_results,
+        channelId=channel_id
+    ).execute()
 
-    video_links = []
     for item in response['items']:
         video_id = item['id']['videoId']
         video_url = f"https://www.youtube.com/watch?v={video_id}"
@@ -22,11 +25,32 @@ def get_video_links(api_key, query, max_results=10):
     return video_links
 
 if __name__ == "__main__":
-    api_key = 'AIzaSyA4CD1mPnWF3kVsUFnEHxAN67qKX8XD08A'  # Replace with your actual API key
-    query = 'climate change'  # Replace with your search keywords
-    df=pd.read_excel('keywords.xlsx')
-    max_results = 100  # Adjust the number of results you want
+    
+    columns = ['Country', 'Channel', 'Keyword', 'Video']
+    df_videos = pd.DataFrame(columns=columns)
 
-    links = get_video_links(api_key, df, max_results)
-    for link in links:
-        print(link)
+    df_keywords = pd.read_excel('keywords.xlsx')
+
+    df_channels = pd.read_excel('countries.xlsx',sheet_name='channels')
+
+    res = []
+
+    for index, channel in df_channels.iterrows():
+        for index, query in df_keywords.iterrows():
+
+            links = get_video_links(query.iloc[0], channel['Channel_ID'], max_results)
+            result = []
+            new_row = {
+                'Country': channel['Country'],
+                'Channel': channel['Party'],
+                'Keyword': query.iloc[0],
+                'Video': links
+            }    
+            result.append(new_row)
+            print(result)
+            df_videos = pd.concat([df_videos, pd.DataFrame(result)], ignore_index=True)
+        
+
+#    print(df_videos.describe())
+
+df_videos.to_excel('output.xlsx')
